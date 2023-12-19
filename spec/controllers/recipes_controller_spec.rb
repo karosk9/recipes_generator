df28622e13debc4ab4dd99cc9a10d5841e6c4dd7
Karolina Ośka <k.oska@codeandpepper.com>
2023-12-19 18:50:25 +0100

    [1] prevent calling external api from tests

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -5,6 +5,8 @@ require 'rails_helper'
 RSpec.describe RecipesController, type: :controller do
   describe 'GET #random' do
     it 'returns a successful response' do
+      allow(HTTParty).to receive(:get).and_return({ 'meals' => [{ 'idMeal' => '1' }] })
+
       get :random
       expect(response).to be_successful
     end
@@ -12,6 +14,8 @@ RSpec.describe RecipesController, type: :controller do
 
   describe 'GET #search' do
     it 'returns a successful response' do
+      allow(HTTParty).to receive(:get).and_return({ 'meals' => [{ 'idMeal' => '1' }] })
+
       get :search
       expect(response).to be_successful
     end
@@ -19,6 +23,7 @@ RSpec.describe RecipesController, type: :controller do
 
   describe 'GET #show' do 
     it 'returns a successful response' do
+      allow(HTTParty).to receive(:get).and_return({ 'meals' => [{ 'idMeal' => '1' }] })
       
       get :show, params: { id: 1 }
       expect(response).to be_successful
