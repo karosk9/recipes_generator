716670c568f284da4b23f0a6ab80706a1f6111e9
Karolina Ośka <karosk9@gmail.com>
2023-12-19 19:25:44 +0100

    [1] Meal MVP (#1)
    * [1] Create recippes#random endpoint
    
    * [1] Create views for random endpoint
    
    * [1]  Add search endpoint with view
    
    * [1] Add show endpoint with view
    
    * [1] Add webmock
    
    * [1] prevent calling external api from tests
    
    * [1] Customise index page
    
    * [1] Make rubocop happy
    
    * [1] small fixes
    
    ---------
    
    Co-authored-by: Karolina Ośka <k.oska@codeandpepper.com>

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class RecipesController < ApplicationController
+  def random
+    @meal = call_api['meals'].first
+  end
+
+  def search
+    @meals = HTTParty.get("https://www.themealdb.com/api/json/v1/1/search.php?f=#{params[:query]}")
+  end
+
+  def show
+    @meal = HTTParty.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=#{params[:id]}")
+  end
+
+  private
+
+  def call_api
+    HTTParty.get('https://www.themealdb.com/api/json/v1/1/random.php')
+  end
+end
--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -5,7 +5,27 @@ require 'rails_helper'
 RSpec.describe RecipesController, type: :controller do
   describe 'GET #random' do
     it 'returns a successful response' do
+      allow(HTTParty).to receive(:get).and_return({ 'meals' => [{ 'idMeal' => '1' }] })
+
       get :random
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #search' do
+    it 'returns a successful response' do
+      allow(HTTParty).to receive(:get).and_return({ 'meals' => [{ 'idMeal' => '1' }] })
+
+      get :search
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #show' do
+    it 'returns a successful response' do
+      allow(HTTParty).to receive(:get).and_return({ 'meals' => [{ 'idMeal' => '1' }] })
+
+      get :show, params: { id: 1 }
       expect(response).to be_successful
     end
   end
