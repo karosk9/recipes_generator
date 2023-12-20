1e3980a225bd4bd7269408272c367e5484b74bbd
Karolina Ośka <k.oska@codeandpepper.com>
2023-12-20 16:19:52 +0100

    [4] Handle net connection errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  HTTP_ERRORS = [
+    EOFError,
+    Errno::ECONNRESET,
+    Errno::EINVAL,
+    Net::HTTPBadResponse,
+    Net::HTTPHeaderSyntaxError,
+    Net::ProtocolError,
+    Timeout::Error,
+    SocketError
+  ].freeze
+
+  rescue_from(*HTTP_ERRORS, with: :handle_api_error)
+
+  private
+
+  def handle_api_error(error)
+    Rails.logger.error "[ERROR] Failed to connect with API error: #{error.message}"
+    Rails.logger.error error.backtrace
+
+    redirect_to home_index_path, alert: I18n.t('errors.alerts.http_error')
+  end
 end
