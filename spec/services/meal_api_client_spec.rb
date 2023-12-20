ed52f79d24b5a8c3265db4a7033d998e2d5f3fa2
Karolina Ośka <k.oska@codeandpepper.com>
2023-12-20 14:37:39 +0100

    [2] Create MealApiClient for clearer communication with API

---
+++ spec/services/meal_api_client_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MealApiClient, type: :service do
+  describe '#random' do
+    it 'returns JSON response from external API' do
+      api_response = JSON.parse(
+        Rails.root.join('spec/fixtures/random_recipe_response.json').read
+      )
+      allow(HTTParty).to receive(:get).and_return(api_response)
+
+      expect(subject.random).to eq(api_response['meals'].first)
+    end
+  end
+
+  describe '#meal_by_id' do
+    it 'returns JSON response from external API' do
+      api_response = JSON.parse(
+        Rails.root.join('spec/fixtures/random_recipe_response.json').read
+      )
+      allow(HTTParty).to receive(:get).and_return(api_response)
+
+      expect(subject.meal_by_id(id: 52_854)).to eq(api_response['meals'].first)
+    end
+  end
+
+  describe '#alphabetic_search' do
+    it 'returns JSON response from external API' do
+      api_response = JSON.parse(
+        Rails.root.join('spec/fixtures/random_recipe_response.json').read
+      )
+      allow(HTTParty).to receive(:get).and_return(api_response)
+
+      expect(subject.alphabetic_search(letter: 'p')).to eq(api_response['meals'])
+    end
+  end
+end
