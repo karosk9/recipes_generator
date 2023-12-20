71003e3f37ac5dec9cfda7c9c02a758a546d7482
Karolina Ośka <k.oska@codeandpepper.com>
2023-12-20 15:56:19 +0100

    [2] Add webmock missing config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,9 @@
 require 'webmock/rspec'
 
 RSpec.configure do |config|
+  config.before(:each) do
+    WebMock.disable_net_connect!(allow_localhost: true)
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
